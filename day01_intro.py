01423deeb03c3982bd7d500719d64dad6c642dd7
Dustin Van Dyne <182355087+DD8631@users.noreply.github.com>
2025-05-02 11:56:51 -0500

    Day 01: Input, ooutput, string methods, slicing

---
+++ day01_intro.py
@@ -0,0 +1,8 @@
+first_name = input('What is your First Name\n')
+fav_prog_lang= input('What is your favorite programming language\n')
+
+print(f'Nice to meet you {first_name}! {fav_prog_lang} is a great choice!')
+print(f'Your name has {len(first_name)} characters.')
+
+print(f'{first_name.upper()}')
+print(fav_prog_lang[::-1])
