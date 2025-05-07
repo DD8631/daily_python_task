013e3bff077466c2fc118f88e2e908f0acb78891
Dustin Van Dyne <182355087+DD8631@users.noreply.github.com>
2025-05-07 07:58:30 -0500

    Day 02: Review of Day 01 concepts

---
+++ day02_review.py
@@ -0,0 +1,6 @@
+animal = input('What is your favorite animal?\n')
+animal_rev = animal[::-1]
+
+print(f'Your favorite animal is {animal.upper()}.')
+print(animal_rev)
+print(f'The number of characters in {animal} is {len(animal)}.')
