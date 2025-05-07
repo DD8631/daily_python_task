9e0bc0e9bb36d769d2baf1a53171cd9d404a334e
Dustin Van Dyne <182355087+DD8631@users.noreply.github.com>
2025-05-07 06:36:14 -0500

    Day 1 complete.

--- day01_intro.py
+++ day01_intro.py
@@ -1,8 +1,11 @@
+# Inputs...
+# Variables
 first_name = input('What is your First Name\n')
 fav_prog_lang= input('What is your favorite programming language\n')
 
+# Outputs...
 print(f'Nice to meet you {first_name}! {fav_prog_lang} is a great choice!')
 print(f'Your name has {len(first_name)} characters.')
 
-print(f'{first_name.upper()}')
-print(fav_prog_lang[::-1])
+print(f'{first_name.upper()}') # Print all uppercase (which is called a method)
+print(fav_prog_lang[::-1]) # Print in reverse using slicing
